c10f48c14c1b56fc882315228bbf27dbafa3a528
bclavijo110430 <58958518+bclavijo110430@users.noreply.github.com>
2021-10-11 15:15:37 -0500

    add install-docker

---
+++ install-docker.sh
@@ -0,0 +1,26 @@
+sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+ssh-keygen -A
+service ssh restart
+mkdir /root/.ssh/
+cat /mnt/c/cert/id_rsa.pub >> /root/.ssh/authorized_keys
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update & apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo rm /etc/resolv.conf
+sudo bash -c 'echo "nameserver 8.8.8.8" > /etc/resolv.conf'
+sudo bash -c 'echo "[network]" > /etc/wsl.conf'
+sudo bash -c 'echo "generateResolvConf = false" >> /etc/wsl.conf'
+sudo chattr +i /etc/resolv.conf
+sudo service docker start
+sudo service ssh start
