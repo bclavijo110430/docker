9b32c1d83abe775486ba2fdad7bbbfd6d2e6cd2b
bclavijo110430 <58958518+bclavijo110430@users.noreply.github.com>
2021-10-12 11:54:53 -0500

    add mount options

--- install-docker.sh
+++ install-docker.sh
@@ -3,24 +3,28 @@ sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/s
 ssh-keygen -A
 service ssh restart
 mkdir /root/.ssh/
+
 cat /c/cert/id_rsa.pub >> /root/.ssh/authorized_keys
+
 sudo rm /etc/resolv.conf
 sudo bash -c 'echo "nameserver 8.8.8.8" > /etc/resolv.conf'
 sudo bash -c 'echo "[network]" > /etc/wsl.conf'
 sudo bash -c 'echo "generateResolvConf = false" >> /etc/wsl.conf'
+sudo bash -c 'echo "[automount]" > /etc/wsl.conf'
+sudo bash -c 'echo "root = /" > /etc/wsl.conf'
+sudo bash -c 'echo "options = "metada" > /etc/wsl.conf'
 sudo chattr +i /etc/resolv.conf
 
+sudo apt-get update
 
-apt-get install \
+sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release -y
+    lsb-release
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
