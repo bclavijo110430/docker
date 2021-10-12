2fc602642c4c6ec279a5daea6effea3dd0038bb0
bclavijo110430 <58958518+bclavijo110430@users.noreply.github.com>
2021-10-12 10:47:15 -0500

    change mount point

--- install-docker.sh
+++ install-docker.sh
@@ -4,6 +4,12 @@ ssh-keygen -A
 service ssh restart
 mkdir /root/.ssh/
 cat /c/cert/id_rsa.pub >> /root/.ssh/authorized_keys
+sudo rm /etc/resolv.conf
+sudo bash -c 'echo "nameserver 8.8.8.8" > /etc/resolv.conf'
+sudo bash -c 'echo "[network]" > /etc/wsl.conf'
+sudo bash -c 'echo "generateResolvConf = false" >> /etc/wsl.conf'
+sudo chattr +i /etc/resolv.conf
+
 
 apt-get install \
     apt-transport-https \
@@ -21,10 +27,6 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update & apt-get install docker-ce docker-ce-cli containerd.io -y
-sudo rm /etc/resolv.conf
-sudo bash -c 'echo "nameserver 8.8.8.8" > /etc/resolv.conf'
-sudo bash -c 'echo "[network]" > /etc/wsl.conf'
-sudo bash -c 'echo "generateResolvConf = false" >> /etc/wsl.conf'
-sudo chattr +i /etc/resolv.conf
+
 sudo service docker start
 sudo service ssh start
